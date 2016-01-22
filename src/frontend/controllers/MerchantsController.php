209f907d653ed81fc2b5ed5ca2bacdaa38941278
zy <635862829@qq.com>
2016-01-22 14:58:06 +0800

    sa

--- src/frontend/controllers/MerchantsController.php
+++ src/frontend/controllers/MerchantsController.php
@@ -7,15 +7,41 @@ use yii\web\BadRequestHttpException;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
+use frontend\models\students\MerType;
+use frontend\models\students\Circles;
+use frontend\models\students\MerBase;
 
 /**
  * 优质商家
  */
 class MerchantsController extends Controller
 {
+	/*首页*/
     public function actionIndex()
     {
-        return $this->render('index');
+    	/*分类*/
+    	$models_ty 	= new MerType;
+    	$types 		= $models_ty->getType();
+    	/*商圈*/
+    	$models_cir = new Circles;
+    	$circles 	=  $models_cir->getCircle();
+    	/*优质商家*/
+    	$models_go 	= new  MerBase;
+    	$mers		= $models_go->getMerInfo();
+        return $this->render('index',
+        		[
+        			'types'		=>$type,
+        			'circles'	=>$circles,
+        			'mers'		=>$mers,
+        		]);
     }
+    /*商家详细信息*/
+    /*public function actionDetails()
+    {
+    	$mer_id 	= Yii::$app->request->get('mer_id');
+    	$model 		= new MerBase;
+    	$mer_detail = $model->getDetail($mer_id);
+    	return $this->render();
+    }*/
 
 }
