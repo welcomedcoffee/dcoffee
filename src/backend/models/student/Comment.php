128f61a24630d801e823f7711d6f86e359f15e69
wangwenxiu <596600892@qq.com>
2016-01-22 21:21:40 +0800

    add

---
+++ src/backend/models/student/Comment.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace backend\models\student;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%fin_comment}}".
+ *
+ * @property integer $comment_id
+ * @property integer $comment_type
+ * @property integer $comment_level
+ * @property string $comment_price
+ * @property integer $model_id
+ * @property integer $comment_addtime
+ * @property integer $user_id
+ * @property string $user_name
+ * @property string $comment_content
+ * @property integer $comment_status
+ */
+class Comment extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%fin_comment}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['comment_id'], 'required'],
+            [['comment_id', 'comment_type', 'comment_level', 'model_id', 'comment_addtime', 'user_id', 'comment_status'], 'integer'],
+            [['comment_price'], 'number'],
+            [['comment_content'], 'string'],
+            [['user_name'], 'string', 'max' => 30]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'comment_id' => 'Comment ID',
+            'comment_type' => 'Comment Type',
+            'comment_level' => 'Comment Level',
+            'comment_price' => 'Comment Price',
+            'model_id' => 'Model ID',
+            'comment_addtime' => 'Comment Addtime',
+            'user_id' => 'User ID',
+            'user_name' => 'User Name',
+            'comment_content' => 'Comment Content',
+            'comment_status' => 'Comment Status',
+        ];
+    }
+}
