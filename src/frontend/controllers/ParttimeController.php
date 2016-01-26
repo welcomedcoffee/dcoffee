5997d08c50ab1ef8de88b293f24d47194413a894
zy <635862829@qq.com>
2016-01-26 15:42:57 +0800

    sadas

--- src/frontend/controllers/ParttimeController.php
+++ src/frontend/controllers/ParttimeController.php
@@ -3,24 +3,52 @@ namespace frontend\controllers;
 
 use Yii;
 use yii\web\Controller;
-
+use app\models\part\FinPartType;
+use app\models\part\FinJobDetails;
+use frontend\models\students\Region;
 /**
  * 兼职机会
  */
 class ParttimeController extends BaseController
 {
 	/*
-     * @inheritdoc 公共部分
+     * @inheritdoc 兼职列表
      */
-    public function common()
-    {
-        $models_ty  = new MerType;
-        $types      = $models_ty    ->getType();
-        return ['types'=>$types];
-    }
     public function actionIndex()
     {
-        return $this->render('index');
+        $models_type    = new FinPartType();  //实例化分类
+        $models_region  = new Region();      //实例化地区
+        $models_part    = new FinJobDetails;//实例化兼职
+        $types          = $models_type->partComment();
+        $region_id      = '52';
+        $regions        = $models_region->getRegion($region_id);
+        //接受查询分类
+        $keyword    = Yii::$app->request->get('1');
+        $new_key    = Yii::$app->request->get('2');
+        if ($new_key) 
+        {
+            foreach ($new_key as $key => $value) 
+            {
+                $keyword[$key] = $value;
+            }
+        }
+        if (empty($keyword['type'])) {
+            $keyword['type'] = 0;
+        }
+        if (empty($keyword['region'])) {
+            $keyword['region'] = 0;
+        }
+        $parts          = $models_part->getParts($keyword);
+        $pages          = $parts['pages'];
+        unset($parts['pages']);
+        return $this->render('index',
+                [
+                    'types'     => $types,
+                    'regions'   => $regions,
+                    'parts'     => $parts,
+                    'keyword'   => $keyword,
+                    'pages'     => $pages,
+                ]);
     }
 
 }
