fcacfcad98678f1d53a2ceeb97640429d97a0128
root <root@iZ256yooq4vZ.(none)>
2016-01-26 21:35:40 +0800

    teste

--- src/backend/models/student/PayOrder.php
+++ src/backend/models/student/PayOrder.php
@@ -60,11 +60,11 @@ class PayOrder extends \yii\db\ActiveRecord
                      -> one();
     }
          //查询
-    public function sn($order_sn)
+    public static function sn($order_sn)
     {
-        return $this -> find()
+        return self::find()
                      -> where(['=','order_sn',$order_sn])
-                     -> asArray()
+                 //    -> asArray()
                      -> one();
     }
    
