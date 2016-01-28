09e5abd3b9d01813f1bde9e007d1f2bdf433ee83
xht <974500380@qq.com>
2016-01-28 16:37:41 +0800

    123

--- src/frontend/controllers/ConsumptionController.php
+++ src/frontend/controllers/ConsumptionController.php
@@ -10,6 +10,9 @@ use frontend\models\consumption\Bill;
 use frontend\models\consumption\Payment;
 use common\components\CreateExcel;
 use frontend\models\consumption\Preferential;
+use app\models\part\FinRegion;
+use frontend\models\store\MerType;
+use frontend\models\store\MerchantBase;
 use yii\data\Pagination;
 /**
  * 消费商家
@@ -227,7 +230,37 @@ class ConsumptionController extends BaseController
 	//基本资料
 	public function actionBasedata()
 	{
-		return $this->render('basedata');
+		$model     = new MerchantBase();
+		$session   = Yii::$app->session;
+		$session ->open();
+		$mer_id    = $session -> get('user_id') ? $session->get('user_id'):1;
+
+		$model     = $model->findOne($mer_id);
+		//获取分类
+		$MerType   = new MerType();
+		$type      = $MerType -> getType();
+		$childtype = $MerType -> getChildType($model->ind_type);
+
+		/* 查询省份 */
+		$region    = new FinRegion();
+		$province  = $region->getProvince();
+		//print_r($model);die;
+		//print_r($model->mer_province);die;
+		$citys     = $region->getRegion($model->mer_province);
+		//print_r($citys);die;
+		$areas     = $region->getRegion($model->mer_city);
+
+		//是否被修改
+		$is_update = Yii::$app->request->get("re");
+        return $this->render('basedata',[
+										'model'    =>$model,
+										'province' =>$province,
+										'citys'    =>$citys,
+										'areas'    =>$areas,
+										'type'     =>$type,
+										'childtype'=>$childtype,
+										'is_update'=>$is_update
+                                    ]);
 	}
 	//账户安全
 	public function actionSecurity()
@@ -247,4 +280,80 @@ class ConsumptionController extends BaseController
 	{
 		return $this->render('limoney');
 	}
+
+	/**
+     * 接受临时文件
+     */
+    public function actionUpload()
+    {
+    	if(count($_FILES) > 0)
+		{
+			$f = $_FILES['file'];
+			$filename = 'upload/' . md5(uniqid(rand())) . '_' . $f['name'];
+			move_uploaded_file($f['tmp_name'], $filename);
+			echo "/".$filename;
+		}
+		else
+		{
+			echo 'no files';
+		}
+
+    }
+
+    /**
+     * @return string
+     * 获取地区信息
+     */
+    public function actionRegion()
+    {
+		$region_id = Yii::$app->request->get("region_id");
+		$type      = Yii::$app->request->get("type");
+		$region    = new FinRegion();
+		$getregion = $region->getRegion($region_id);
+		$option    = "";
+        foreach($getregion as $k=>$v)
+        {
+            $option.="<option class='".$type."' value='".$v['region_id']."'>".$v['region_name']."</option>";
+        }
+        echo $option;
+    }
+    /**
+     * @return string
+     * 获取子行业分类
+     */
+    public function actionMertype()
+    {
+		$type_id = Yii::$app->request->get("type_id");
+		$Mertype = new Mertype();
+		$type    = $Mertype->getChildType($type_id);
+		$option  = "";
+        foreach($type as $k=>$v)
+        {
+            $option.="<option value='".$v['type_id']."'>".$v['type_name']."</option>";
+        }
+        echo $option;
+    }
+
+    /**
+     * 修改商家信息
+     */
+    public function actionUpdate()
+    {
+		$MerchantBase = new MerchantBase();
+		$data = Yii::$app->request->post();
+
+		$session = Yii::$app->session;
+		$session ->open();
+		$mer_id  = $session -> get('user_id') ? $session->get('user_id'):1;
+		$model   = $MerchantBase->findOne($mer_id);
+		$model   -> setAttributes($data['MerchantBase']);
+		$model   -> setAttributes($data);
+		$model   -> validate();
+		$re = $model ->save();
+		//print_r(Yii::$app->request->post());die;
+		if ($re) {
+			$this->redirect(array('/consumption/basedata','re'=>$re));
+		}
+
+    }
 }
--- src/frontend/views/consumption/basedata.php
+++ src/frontend/views/consumption/basedata.php
@@ -1,1 +1,403 @@
 <?php echo $this->render('_consumptionHeader') ?>
+<?php
+	use yii\helpers\Html;
+	use yii\helpers\Url;
+	use yii\widgets\ActiveForm;
+  $this->title = "我的门店";
+?>
+
+		<div class="mt_ri t_ri">
+			<div class="mt_rli">
+				<div class="right">
+					<div class="tittle">
+						<span>企业信息</span> <span class="wenxinart" style="color:red">您好，修改资料任意资料都要重新审核，建议您不要频繁修改，以免给您带来不便。</span>
+					</div>
+					<!-- <form class="form" method="post" action="<?php echo Url::to(['store/update'])?>"> -->
+					<?php
+                $form = ActiveForm::begin([
+                    'options' => ['id'=>'site-form','enctype' => 'multipart/form-data','class' => "form"],
+                    'action'=>Url::to(['store/update']),
+                    'method'=>'post',
+                    'fieldConfig' => [
+                        'template' => '<p class="mar">{label}{input}{error}</p>'
+                    ],
+                ]);
+                ?>
+
+							<!-- <input class="validate[required]" type="text" id="enterprisename" name="enterprisename" placeholder="请输入企业名称"> -->
+							<?= $form->field($model, 'mer_name', ['inputOptions'=>['placeholder'=>'请输入企业名称']])->textInput(['id'=>"enterprisename",'class'=>"validate[required]",'required'=>"required"]) ?>
+
+							<?= $form->field($model, 'mer_contact',['inputOptions'=>['placeholder'=>'请输入联系人'],'template'=>'<p class="mar" style="text-indent: 1em">{label}{input}{error}</p>'])->textInput(['id'=>'contactperson','required'=>"required"]) ?>
+						<!-- <p class="mar" style="text-indent: 1em">
+							联系人：<input class="validate[required]" type="text" id="contactperson" name="contactperson" placeholder="请输入联系人">
+						</p> -->
+						<!-- <p class="mar">
+							联系电话：<input class="validate[required]" type="text" id="contactpersonphone" name="contactpersonphone" placeholder="请输入联系电话">
+						</p> -->
+						<?= $form->field($model, 'mer_conphone', ['inputOptions'=>['placeholder'=>'请输入联系电话']])->textInput(['id'=>"contactpersonphone",'class'=>"validate[required]",'required'=>"required"]) ?>
+
+						<?= $form->field($model, 'mer_position',['inputOptions'=>['placeholder'=>'请输入联系人'],'template'=>'<p class="mar" style="text-indent: 2em">{label}{input}{error}</p>'])->textInput(['id'=>'position','required'=>"required"]) ?>
+						<!-- <p class="mar" style="text-indent: 2em">
+							职位： <input class="validate[required]" type="text" id="position" name="position" placeholder="请输入职位名称">
+						</p> -->
+						<p class="mar">
+							企业地址：
+									<!-- 选择省 -->
+									<select id="province" name="mer_province">
+						 				<option value="-1">请选择</option>
+						 				<?php foreach($province as $k=>$v){
+						  				if ($v['region_id'] == $model->mer_province) {
+						  						echo '<option selected class="province" value="'.$v['region_id'].'">'.$v['region_name'].'</option>';
+						  				}else{
+						  						echo '<option  class="province" value="'.$v['region_id'].'">'.$v['region_name'].'</option>';
+						  				}
+	    							} ?>
+
+						 			</select>
+						 			<input type="text" value="" id="oneCity" class="oneCity" code="" style="display: none">
+						 			<!-- 市 -->
+						 			<select id="city" name="mer_city">
+						 				<option value="-1">请选择</option>
+						 				<?php foreach($citys as $k=>$v){
+						  				if ($v['region_id'] == $model->mer_city) {
+						  						echo '<option selected class="citys" value="'.$v['region_id'].'">'.$v['region_name'].'</option>';
+						  				}else{
+						  						echo '<option  class="citys" value="'.$v['region_id'].'">'.$v['region_name'].'</option>';
+						  				}
+	    							} ?>
+
+						 			</select>
+
+						  		<select id="area" name="mer_area">
+						  			<option value="-1">请选择</option>
+						  			<?php foreach($areas as $k=>$v){
+						  				if ($v['region_id'] == $model->mer_area) {
+						  						echo '<option selected class="areas" value="'.$v['region_id'].'">'.$v['region_name'].'</option>';
+						  				}else{
+						  						echo '<option  class="areas" value="'.$v['region_id'].'">'.$v['region_name'].'</option>';
+						  				}
+	    							} ?>
+						  		</select>
+						</p>
+
+							<!-- <input class="validate[required]" required="required" type="text" id="address" name="mer_address" style="margin-left: 63px" placeholder="请输入企业地址"> -->
+						<?= $form->field($model, 'mer_address',['inputOptions'=>['placeholder'=>'请输入企业地址'],'template'=>'<p class="mar">{input}{error}</p>'])->textInput(['id'=>'position','required'=>"required",'style'=>"margin-left: 63px"]) ?>
+
+						<p class="mar">
+							行业类型：
+									<select name="ind_type" id="industrytype">
+										<option value="-1">请选择行业类型</option>
+										<?php foreach ($type as $k => $v) {
+											if ($v['type_id']==$model->ind_type) {
+												echo "<option selected value=".$v['type_id'].">".$v['type_name']."</option>";
+											}else{
+												echo "<option value=".$v['type_id'].">".$v['type_name']."</option>";
+											}
+
+										} ?>
+									</select>
+									<select id="industChild" name="ind_type_child" style="width: 130px;">
+										<option value="-1">请选择子行业类型</option>
+										<?php foreach ($childtype as $k => $v) {
+											if ($v['type_id']==$model->ind_type_child) {
+											echo "<option selected value=".$v['type_id'].">".$v['type_name']."</option>";
+										}else{
+											echo "<option  value=".$v['type_id'].">".$v['type_name']."</option>";
+											}} ?>
+									</select>
+						</p>
+						<div>
+							<div style="display: inline; position: relative; top: -50px">上传资质：</div>
+							<div class="image" style="margin-top: 0;">
+								<w> 企业LOGO</w>
+								<div class="input-file">
+								<?php
+											if ($model->mer_logo) {
+										 		echo '<img width="120" height="80" id="myselfpic1" src="'.$model->mer_logo.'">
+													<input type="hidden" name="mer_logo" value="'.$model->mer_logo.'">';
+										 	}else{
+										 		echo '<img src="/public/images/qiyelogo.png" width="120" height="80"  id="myselfpic1">
+													<input type="hidden" name="mer_logo" value="">';
+										 	}
+										?>
+
+								</div>
+							</div>
+							<div class="image">
+								<w>营业执照</w>
+								<div class="input-file">
+								<?php
+											if ($model->mer_license) {
+										 		echo '<img width="120" height="80" id="myselfpic2" src="'.$model->mer_license.'">
+													<input type="hidden" name="mer_license" value="'.$model->mer_license.'">';
+										 	}else{
+										 		echo '<img src="/public/images/zhizhao.png" width="120" height="80"  id="myselfpic2">
+													<input type="hidden" name="mer_license" value="">';
+										 	}
+										?>
+
+								</div>
+							</div>
+							<div class="image">
+								<w>税务登记证</w>
+								<div class="input-file">
+								<?php
+											if ($model->mer_registration) {
+										 		echo '<img width="120" height="80" id="myselfpic3" src="'.$model->mer_registration.'">
+													<input type="hidden" name="mer_registration" value="'.$model->mer_registration.'">';
+										 	}else{
+										 		echo '<img src="/public/images/shuiwu.png" width="120" height="80"  id="myselfpic3">
+													<input type="hidden" name="mer_registration" value="">';
+										 	}
+										?>
+
+
+ 						</div>
+							</div>
+							<div class="image">
+								<w>组织机构代码证</w>
+								<div class="input-file">
+								<?php
+											if ($model->mer_allow) {
+										 		echo '<img width="120" height="80" id="myselfpic4" border="0" src="'.$model->mer_allow.'">
+													<input type="hidden" name="mer_allow" value="'.$model->mer_allow.'">';
+										 	}else{
+										 		echo '<img src="/public/images/jigou.jpg" width="120" height="85" border="0" id="myselfpic4">
+													<input type="hidden" name="mer_allow" value="">';
+										 	}
+										?>
+
+								</div>
+
+							</div>
+							<p class="warm">只能上传jpg、jpeg、png类型的图片，大小不能超过2M</p>
+							<div style="position: relative; left: -1em;">
+								<div style="display: inline-block; position: relative; top: -50px;">上传身份证：</div>
+								<div class="imageq" style="margin: 0; display: inline-block;">
+									<w>身份证背面</w>
+									<div class="input-file">
+									<?php
+											if ($model->mer_reverse) {
+										 		echo '<img width="120" height="85" id="myselfpic6" border="0" src="'.$model->mer_reverse.'">
+													<input type="hidden" name="mer_reverse" value="'.$model->mer_reverse.'">';
+										 	}else{
+										 		echo '<img src="/public/images/zhenmian.png" width="120" height="85" border="0" id="myselfpic6">
+													<input type="hidden" name="mer_reverse" value="">';
+										 	}
+										?>
+
+									</div>
+								</div>
+								<div class="imageq" style="display: inline-block;">
+									<w>身份证正面</w>
+									<div class="input-file">
+									<?php
+											if ($model->mer_positive) {
+										 		echo '<img width="120" height="85" id="myselfpic5" border="0" src="'.$model->mer_positive.'">
+													<input type="hidden" name="mer_positive" value="'.$model->mer_positive.'">';
+										 	}else{
+										 		echo '<img src="/public/images/fanmian.jpg" width="120" height="85" border="0" id="myselfpic5" class=" ">
+													<input type="hidden" name="mer_positive" value="">';
+										 	}
+										?>
+
+									</div>
+								</div>
+								<p class="warm1">只能上传jpg、jpeg、png类型的图片，大小不能超过2M</p>
+							</div>
+							<p class="mar">
+								公司介绍：
+								<textarea class="validate[required]" id="companyinfo" name="mer_introduce" style="width: 336px; height: 100px;"><?= $model->mer_introduce ?></textarea>
+								<span style="position: relative; left: -90px; top: 40px; color: #e5e5e4">最多输入500字</span>
+							</p>
+							<div>
+								<div style="display: inline-block; position: relative; top: -50px">企业图片：</div>
+								<div class="imagew" style="display: inline-block;">
+									<w>企业图片</w>
+									<div class="input-file">
+										<?php
+											if ($model->mer_image1) {
+										 		echo '<img width="120" height="80" id="myselfpic7" class="" src="'.$model->mer_image1.'">
+													<input type="hidden" name="mer_image1" value="'.$model->mer_image1.'">';
+										 	}else{
+										 		echo '<img width="120" height="80" id="myselfpic7" class="" src="/public/images/logo.png">
+													<input type="hidden" name="mer_image1" value="">';
+										 	}
+										?>
+
+									</div>
+								</div>
+								<div class="imagew" style="display: inline-block;">
+									<w>企业图片</w>
+									<div class="input-file">
+										<?php
+											if ($model->mer_image2) {
+										 		echo '<img width="120" height="80" id="myselfpic8" class=" " src="'.$model->mer_image1.'">
+													<input type="hidden" name="mer_image2" value="'.$model->mer_image1.'">';
+										 	}else{
+										 		echo '<img width="120" height="80" id="myselfpic8" class=" " src="/public/images/logo.png">
+													<input type="hidden" name="mer_image2" value="">';
+										 	}
+										?>
+
+									</div>
+								</div>
+								<div class="imagew" style="display: inline-block;">
+									<w>企业图片</w>
+									<div class="input-file">
+									<?php
+											if ($model->mer_image3) {
+										 		echo '<img width="120" height="80" id="myselfpic9" class=" " src="'.$model->mer_image1.'">
+													<input type="hidden" name="mer_image3" value="'.$model->mer_image1.'">';
+										 	}else{
+										 		echo '<img width="120" height="80" id="myselfpic9" class=" " src="/public/images/logo.png">
+													<input type="hidden" name="mer_image3" value="">';
+										 	}
+										?>
+
+									</div>
+								</div>
+								<p class="warm">只能上传jpg、jpeg、png类型的图片，大小不能超过2M</p>
+							</div>
+
+							 <input type="submit" value="修改" id="btnSave" style="width: 100px; height: 30px; background: #f39700; color: white; position: relative; left: 105px; margin-top: 3em; border: 0">
+
+						</div>
+					<!-- </form> -->
+					<?php ActiveForm::end();?>
+				</div>
+			</div>
+		</div>
+	</div>
+<div class="qpzz" style="display:<?php if ($is_update==1) {
+	echo '';
+}else{echo 'none';} ?>;">
+   <div class="tip_box">
+    <h3>温馨提示</h3>
+    <img src="/public/images/cross27.png" style="width:25px;height:25px;float: right;position: relative;top:-35px;left:-5px;">
+     <div class="con_t">
+<p id="titleBox">资料提交成功，工作人员会在3个工作日内完成您的资料审核，审核通过后您才可以使用网站上的功能。</p>
+
+     </div>
+     <br><br>
+     <div style="float: right; margin-right: 20px;">
+     	<input type="button" name="" id="studetail" value="确定" style="width: 70px; height: 30px;border-radius:4px;background-color:#0089cf;color: white;">&nbsp;&nbsp;
+
+     </div>
+   </div>
+</div>
+
+<script>
+	//创建input上传标签
+	//上传图片
+	for (var i = 1; i <= $('.input-file').length; i++) {
+
+		new AjaxUpload(a='#myselfpic'+i, {
+                    // 提交目标
+                    action: "<?php echo Url::to(['store/upload'])?>",
+                    // 服务端接收的名称
+                    name: "file",
+                    // 自动提交
+                    autoSubmit: true,
+                    // 选择文件之后…
+                    onChange: function (file, extension) {
+                        if (new RegExp(/(jpg)|(jpeg)|(bmp)|(gif)|(png)/i).test(extension)) {
+                            $("#filepath").val(file);
+                        } else {
+                            alert("只限上传图片文件，请重新选择！");
+                        }
+                    },
+                    // 开始上传文件
+                    onSubmit: function (file, extension) {
+
+                    },
+
+                    // 上传完成之后
+                    onComplete: function (file, response) {
+                        /*if (response == "Success") $("#state").val("上传完成！");
+                        else $("#state").val(response);*/
+                        console.log(this);
+                        this._button.src = response;
+
+                        $("#"+this._button.id).next("input").val(response);
+
+                    }
+
+                });
+	}
+
+	//加载市列表
+  	$("#province").change(function() {
+  		 var region_id = $(this).val();
+  		 var type = "city"
+  		 if (region_id != '-1') {
+  		 		$.ajax({
+  		 			url: "<?= Url::to(['store/region']) ?>",
+  		 			type: "GET",
+  		 			dataType: 'html',
+  		 			data: "region_id="+region_id+"&type="+type,
+  		 			success: function(msg){
+		                $("#city>option:gt(0)").remove();
+		                $("#area>option:gt(0)").remove();
+		                $("#city").append(msg)
+		            }
+		  		})
+  		 }else{
+  		 	$("#city>option:gt(0)").remove();
+  		 	$("#area>option:gt(0)").remove();
+  		 }
+  });
+
+  	//加载区列表
+    $("#city").change(function() {
+  		 var region_id = $(this).val();
+  		 var type = "area"
+  		 if (region_id != '-1') {
+  		 		$.ajax({
+  		 			url: "<?= Url::to(['store/region']) ?>",
+  		 			type: "GET",
+  		 			dataType: 'html',
+  		 			data: "region_id="+region_id+"&type="+type,
+
+  		 			success: function(msg){
+                $("#area>option:gt(0)").remove();
+                $("#area").append(msg);
+            }
+  		 		})
+  		 }
+  });
+    //加载子行业信息
+    $("#industrytype").change(function() {
+  		 var type_id = $(this).val();
+
+  		 if (type_id != '-1') {
+  		 		$.ajax({
+  		 			url: "<?= Url::to(['store/mertype']) ?>",
+  		 			type: "GET",
+  		 			dataType: 'html',
+  		 			data: "type_id="+type_id,
+
+  		 			success: function(msg){
+                		$("#industChild>option:gt(0)").remove();
+                		$("#industChild").append(msg)
+            		}
+  		 		})
+  		 }else{
+  		 	$("#industChild>option:gt(0)").remove();
+  		 }
+  });
+
+    $('#studetail').click(function() {
+    		$('.qpzz').hide('slow/400/fast');
+    });
+
+    $('#btnSave').click(function() {
+    	if (confirm("修改信息后需要重新审核！您确定要修改吗?")) {
+    		return true
+    	}else{
+    		return false
+    	}
+
+    });
+
+</script>
